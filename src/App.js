c68f6d1838c8ffd89eadc99e80001557491f5a26
codenamics <info@codenamics.pl>
2019-01-01 16:47:00 +0100

    stripe up

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import Stripe from "./components/Stripe";
 class App extends Component {
   render() {
     return (
-      // <StripeProvider apiKey="pk_test_A2SjqfOGQAF42tSHcxIk1Zpw">
-      // </StripeProvider>
-      <Router>
-        <React.Fragment>
-          <Route exact path="/" component={Pricing} />
-          <Route exact path="/charge" component={Stripe} />
-        </React.Fragment>
-      </Router>
+      <StripeProvider apiKey="pk_test_A2SjqfOGQAF42tSHcxIk1Zpw">
+        <Router>
+          <React.Fragment>
+            <Route exact path="/" component={Pricing} />
+            <Route exact path="/charge" component={Stripe} />
+          </React.Fragment>
+        </Router>
+      </StripeProvider>
     );
   }
 }
