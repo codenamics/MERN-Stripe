898488007b51b4722563f4c0848d02825f3d224d
codenamics <info@codenamics.pl>
2019-01-06 23:11:00 +0100

    charges route mulit DB

--- server/routes/api/charge.js
+++ server/routes/api/charge.js
@@ -1,83 +1,85 @@
-require('dotenv').load();
-const express = require('express')
-const router = express.Router()
+require("dotenv").load();
+const express = require("express");
+const router = express.Router();
 const stripe = require("stripe")(process.env.SECRET_KEY);
-const Charge = require('../../models/Charge')
+const Charge = require("../../models/Charge");
 router.post("/pay", (req, res) => {
-    const {
-        price,
-        token,
-        email
-    } = req.body
+  const { price, token, email } = req.body;
 
-    stripe.customers.create({
-            email,
-            source: token
-        })
-        .then(customer => stripe.charges.create({
-            amount: price,
-            description: email,
-            currency: 'usd',
-            customer: customer.id,
-            receipt_email: email
-        }))
-        .then(status => {
-            res.json({
-                status
-            })
-        }).catch(err => res.send(err))
+  stripe.customers
+    .create({
+      email,
+      source: token
+    })
+    .then(customer =>
+      stripe.charges.create({
+        amount: price,
+        description: email,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: email
+      })
+    )
+    .then(status => {
+      res.json({
+        status
+      });
+    })
+    .catch(err => res.send(err));
 
-    if (email !== '' || email !== undefined) {
-
-        const output = `
+  if (email !== "" || email !== undefined) {
+    const output = `
       <h1>You just purchase plan ${price}</h1>
       `;
-        let transporter = nodemailer.createTransport({
-            host: process.env.SMTP,
-            port: process.SMTP_PORT,
-            secure: true,
-            auth: {
-                user: process.env.AUTH_USER,
-                pass: process.env.AUTH_PASS
-            },
-            tls: {
-                rejectUnauthorized: false
-            }
-        });
+    let transporter = nodemailer.createTransport({
+      host: process.env.SMTP,
+      port: process.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.AUTH_USER,
+        pass: process.env.AUTH_PASS
+      },
+      tls: {
+        rejectUnauthorized: false
+      }
+    });
 
-        let mailOptions = {
-            from: '"NewHorizon"',
-            to: email,
-            subject: `New stack for ${price}`,
-            html: output
-        };
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            res.status(200).json({
-                msg: "Message has been sent"
-            });
-        });
-    }
+    let mailOptions = {
+      from: '"NewHorizon"',
+      to: email,
+      subject: `New stack for ${price}`,
+      html: output
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      res.status(200).json({
+        msg: "Message has been sent"
+      });
+    });
+  }
 });
 
-router.get('/all', (req, res) => {
-    stripe.charges.list({
-        limit: 3
-    }).then(charges => {
-        charges.data.forEach(charge => {
-            return newData = new Charge({
-                amount: charge.amount,
-            })
-        });
-        newData
-            .save()
-            .then(charge => {
-                res.json(charge)
-            })
-            .catch(err => console.log(err))
+router.get("/all", (req, res) => {
+  stripe.charges
+    .list({
+      limit: 3
     })
-})
+    .then(charges => {
+      let promises = charges.data.map(charge => {
+        let newData = new Charge({ amount: charge.amount });
+        return newData.save();
+      });
+      return Promise.all(promises);
+    })
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.error(err);
+    });
+});
 
 module.exports = router;
